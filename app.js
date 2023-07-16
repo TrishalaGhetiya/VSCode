2198389efcecfa52a2fd4ad89c1159b67da2b3e5
TrishalaGhetiya <115083359+TrishalaGhetiya@users.noreply.github.com>
2023-07-16 18:32:51 +0530

    Added another input box and updated search code

--- app.js
+++ app.js
@@ -10,17 +10,26 @@ function addItem(e)
 {
     e.preventDefault();
     const newItem = document.getElementById('item').value;
+    const newDescription = document.getElementById('description').value;
     const li = document.createElement('li');
     li.className='list-group-item';
     li.appendChild(document.createTextNode(newItem));
+    li.appendChild(document.createTextNode(newDescription));
     //itemList.appendChild(li);
+
+    const editButton = document.createElement('button');
+    editButton.className= 'btn btn-sm btn-success float-right';
+    editButton.appendChild(document.createTextNode('Edit'));
 
     const deleteButton = document.createElement('button');
     deleteButton.className= 'btn btn-danger btn-sm float-right delete';
     deleteButton.appendChild(document.createTextNode('X'));
 
     li.appendChild(deleteButton);
+    li.appendChild(editButton);
     itemList.appendChild(li);
+    newItem.value='';
+    newDescription.value='';
 }
 
 function removeItem(e)
@@ -40,8 +49,10 @@ function filterItem(e)
     const items = itemList.getElementsByTagName('li');
     Array.from(items).forEach(function(item)
     {
-        const itemName = item.firstChild.textContent;
-        if(itemName.stoLowerCase().indexOf(text)!=-1)
+        const ItemName1 = item.firstChild.textContent;
+        const ItemName2 = item.lastChild.textContent;
+        
+        if(ItemName1.toLowerCase().indexOf(text)!=-1 || ItemName2.toLowerCase().indexOf(text)!=-1)
         {
             item.style.display='block';
         }
